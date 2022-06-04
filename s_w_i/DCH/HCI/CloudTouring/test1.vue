b1af0e7329d0dd7f3e948e913936042855513422
GaryDing01 <gding01@qq.com>
2022-06-04 20:51:39 +0800

    CloudFirst

---
+++ s_w_i/DCH/HCI/CloudTouring/test1.vue
@@ -0,0 +1,257 @@
+<template>
+<div class="recommand-content">
+
+    <ul class="recommand-myul">
+
+        <li class="recommand-list" style="">
+
+            <a href="http://tg.dili360.com/zuimei/index/lookphotobig/id/2674.htm">
+
+                <img src="../../../../assets/undersun.png" alt="" style="">
+
+                <span class="mask">
+
+                    <div class="detail">
+
+                        <h3>庄建明</h3>
+
+                        <p>潮州广济桥</p>
+
+                    </div>
+
+                </span>
+
+                <span class="tips-province">广东</span>
+
+            </a>
+
+        </li><li class="recommand-list">
+
+            <a href="http://tg.dili360.com/zuimei/index/lookphotobig/id/2259.htm">
+
+                <img src="../../../../assets/undersun.png" alt="">
+
+                <span class="mask">
+
+                    <div class="detail">
+
+                        <h3>林忠宇</h3>
+
+                        <p>九曲十八弯</p>
+
+                    </div>
+
+                </span>
+
+                <span class="tips-province">新疆</span>
+
+            </a>
+
+        </li><li class="recommand-list">
+
+            <a href="http://tg.dili360.com/zuimei/index/lookphotobig/id/2267.htm">
+
+                <img src="../../../../assets/undersun.png" alt="">
+
+                <span class="mask">
+
+                    <div class="detail">
+
+                        <h3>林忠宇</h3>
+
+                        <p>雪美人</p>
+
+                    </div>
+
+                </span>
+
+                <span class="tips-province" style="display: block;">吉林</span>
+
+            </a>
+
+        </li><li class="recommand-list">
+
+            <a href="http://tg.dili360.com/zuimei/index/lookphotobig/id/2478.htm">
+
+                <img src="../../../../assets/undersun.png" alt="">
+
+                <span class="mask">
+
+                    <div class="detail">
+
+                        <h3>孙圣杰</h3>
+
+                        <p>街景澳门</p>
+
+                    </div>
+
+                </span>
+
+                <span class="tips-province" style="display: block;">澳门</span>
+
+            </a>
+
+        </li><li class="recommand-list">
+
+            <a href="http://tg.dili360.com/zuimei/index/lookphotobig/id/1577.htm">
+
+                <img src="../../../../assets/undersun.png" alt="">
+
+                <span class="mask">
+
+                    <div class="detail">
+
+                        <h3>马克温</h3>
+
+                        <p>水墨沙江</p>
+
+                    </div>
+
+                </span>
+
+                <span class="tips-province" style="display: block;">福建</span>
+
+            </a>
+
+        </li><li class="recommand-list">
+
+            <a href="http://tg.dili360.com/zuimei/index/lookphotobig/id/2335.htm">
+
+                <img src="../../../../assets/undersun.png" alt="">
+
+                <span class="mask">
+
+                    <div class="detail">
+
+                        <h3>潘龙</h3>
+
+                        <p>《黄河入海口晨曲》</p>
+
+                    </div>
+
+                </span>
+
+                <span class="tips-province">山东</span>
+
+            </a>
+
+        </li>
+
+    </ul>
+
+    <!-- <ul class="myclass">
+
+        <li>测试文本1</li>
+
+        <li>测试文本2</li>
+
+        <li>测试文本3</li>
+
+    </ul> -->
+
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+/* /* *{margin:0;padding:0;font-family: "Microsoft Yahei";list-style:none;transition:top .3s ease;color:inherit;text-decoration: none;} */
+img{width:100%;height:100%;}
+.bg{position:absolute;left:0;top:0;}
+.bg img{display:block;}
+.hidden{text-indent: -9999px;} */
+/* .nanhai{width:160px;height:185px;position:absolute;right:-5px;bottom:0px;}
+.yahai{width: 221px;
+  height: 355px;
+  position: absolute;
+  right: 152px;
+  bottom: 39px;}
+.nanhai img{width:100%;height:100%;}
+.yahai img{width:100%;height:100%;} */
+/* body{background:url(../../images/zuimei/body-bg.jpg)no-repeat;width:100%;height:100%;background-attachment: scroll;background-position:top center;} */
+/*导航图*/
+/* .header{width:100%;height:599px;background:url(../../images/zuimei/CNG-BG.jpg)no-repeat;background-size:100% 100%;position:relative;} */
+/* .header-title{width:556px;height:178px;position: absolute;left:14%;top:35%;}
+.header-logo{width:169px;height:54px;position:absolute;right:0;top:0;}
+.header-circlepath{width:562px;height:484px;position: absolute;right:0;bottom:0;color:#fff;font-weight: 400;letter-spacing:1px;} */
+
+/*导航图结束*/
+/*内容部分开始*/
+
+/* #wrap{width:1300px;height:1033px;position:absolute;}
+#Maps-mask{width:1300px;height:1033px;position:relative;float:left;}
+#Maps{width:1265px;margin:10px auto;position:relative;} */
+.Maps-title-img{width:249px;height:62px;margin:0 auto;}
+#recommand{width:1265px;margin:56px auto;position:relative;overflow: hidden;}
+#recommand .recommand-content li,
+#recommand .recommand-content span{width:253px;}
+#recommand .recommand-content li a{width: 100%;height: 100%;display: block;text-align: center;}
+#recommand .recommand-content{overflow: hidden;margin-top:23px;}
+#recommand .recommand-content li{width:253px; height:248px;float:left;background:#ECECEC;overflow: hidden;}
+#recommand .recommand-content li img{width:auto;position: relative;height: 248px;}
+/* .map-title{display:block;width:370px;margin:0 auto;position:absolute;left:449px;top:135px;z-index: 1001;}
+.recommand-title{width:249px;height:62px;margin:0 auto;text-align: center;} */
+.recommand-list{position:relative;text-align: center;overflow:hidden;}
+.recommand-list:hover .mask{top:0;}
+.detail h3{font-size: 20px;font-weight: lighter;list-style: 25px;width: 180px;margin: 110px auto 0;padding-bottom: 5px;border-bottom: 1px solid rgba(255, 255, 255, 0.7);}
+/* .detail p{padding-top:20px;}
+.container{width:1300px;margin:0 auto;overflow: hidden;} */
+/*内容部分结束*/
+footer部分开始
+ /* .footer{height:213px;background:#444444;margin-top: -50px;} */
+/*小零件部分*/
+/* .china-center{display:block;width:20px;height:20px;position:absolute;z-index: 1001;}
+.tips{display:block;width:34px;height:45px;overflow: hidden;position:absolute;z-index: 1001;}
+.btn:hover{background:#00BEFF;color:#fff;}
+.btn{display: inline-block;cursor: pointer;transition: all .3s ease;font-weight: 900;width: 150px;
+height: 43px;
+line-height: 43px;
+text-align: center;}
+.btn-active{background:#00BEFF;color:#fff;}  */
+.province{z-index: 1000;position:absolute;font-size:16px;cursor: pointer;}
+.mask{position:absolute;left:0;top:100%;width:100%;height:100%;background:rgba(0,0,0,.5);color:#fff;}
+/* .circle{width:36px;height:34px;display:block;position:absolute;background:url(../../images/zuimei/circle.png)no-repeat;z-index: 100000;left:0;top:0;cursor: pointer;} */
+/* .circle-font{width:190px;height:48px;display:block;position:absolute;z-index: 10000;cursor: pointer;transition: color .2s ease;
+text-shadow:0 0 5px #000, 0 0 10px #000, 0 0 8px #000, 0 0 40px #060606, 0 0 70px #060606;;
+}
+.circle-font:hover{color:#eacc3a;font-weight: 900;font-size: 17px;}
+.joincontest{position:absolute;left:21.5%;top:70%;border:1px solid red;padding:6px;color:#fff;}
+.joincontest:hover .joincontest-font{background: orange;}
+.joincontest-font{display:block;padding:10px 42px;background:red;cursor: pointer;transition: all .5s ease;font-size: 18px;}
+
+#share{width:240px;margin:-23px auto;padding-left:122px;display:none;}
+.share-title{height:44px;line-height: 44px;float:left;display:inline-block;} */
+
+.tips-province
+{
+position:absolute;
+left:5px;
+bottom:5px;
+color:#fff;
+text-align: left;
+font-size:18px;
+}
+
+/* .footer{height:213px;background:#444444;color:#fff;margin-top:33px;}
+
+.lowedition{background: yellow;color: #810000;font-size:12px;padding: 4px 0;text-align: center;position:absolute;left:0;top:0;width: 100%;display: none;z-index: 99;} */
+
+
+.recommand-myul{
+    width:20%;
+}
+
+.recommand-myul li{
+    float: left;
+    margin-right: 0px;
+}
+
+.myclass li{
+        float: left;
+        margin-right: 50px;
+        list-style: none;
+    }
+
+
+</style>
